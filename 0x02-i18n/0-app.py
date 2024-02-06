ff4ea1d2a41176f1bc9746f70ae68ceca6ac49bb
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-06 15:55:52 +0000

    Fix Task 0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def home() -> str:
     """ Home Route """
-    return (render_templage("0-index.html"))
+    return (render_template("0-index.html"))
 
 
 if __name__ == "__main__":
-    app.run()
+    host = '0.0.0.0'
+    port = 5000
+    app.run(host=host, port=port, threaded=True)
