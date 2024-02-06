13e3bdc4f154013596435bb4fbbaf69622c48600
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-06 14:36:09 +0000

    Task 0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" Setup a basic Flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """ Home Route """
+    return (render_templage("0-index.html"))
+
+
+if __name__ == "__main__":
+    app.run()
