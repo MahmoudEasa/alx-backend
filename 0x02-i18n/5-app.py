845d76d3666f28368f9b7b9780f75adcd9e19fdf
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-06 19:09:59 +0000

    Test Task 0

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,1 +1,47 @@
 #!/usr/bin/env python3
+""" Setup a basic Flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """ Class to configure available languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Get Locale Function """
+    locale = request.args.get("locale", None)
+    if locale and locale in Config.LANGUAGES:
+        return (locale)
+
+    return (request.accept_languages.best_match(app.config["LANGUAGES"]))
+
+
+@app.route('/')
+def home() -> str:
+    """ Home Route """
+    return (render_template("4-index.html"))
+
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = 5000
+    app.run(host=host, port=port, threaded=True)
