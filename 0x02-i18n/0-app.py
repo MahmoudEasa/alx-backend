4358fca65079f52009ad9ca48686c5c246e4ee69
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-06 19:22:08 +0000

    Fix Test Task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,24 +1,4 @@
 #!/usr/bin/env python3
-""" A script for basic flask integration"""
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    """This function returns the rendered template for the index.html page.
-
-    Returns:
-        The rendered template for the index.html page.
-    """
-    return render_template('0-index.html')
-
-if __name__ == '__main__':
-    app.run()
-
-'''
-#!/usr/bin/env python3
 """ Setup a basic Flask app
 """
 from flask import Flask, render_template
@@ -35,4 +15,4 @@ def home() -> str:
 if __name__ == "__main__":
     host = '0.0.0.0'
     port = 5000
-    app.run(host=host, port=port, threaded=True)'''
+    app.run(host=host, port=port, threaded=True)
