dfc2594258d201ab2d036ddf3267f211e0aa2084
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-06 16:07:55 +0000

    Task 1. Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+""" Setup a basic Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Class to configure available languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home() -> str:
+    """ Home Route """
+    return (render_template("1-index.html"))
+
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = 5000
+    app.run(host=host, port=port, threaded=True)
