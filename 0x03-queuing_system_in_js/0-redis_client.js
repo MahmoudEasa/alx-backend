9f17ec0de39823f652fbcf8879c0c8664c38eb1a
MahmoudEasa <mu01011422865@gmail.com>
2024-03-05 17:55:53 +0200

    Task 1. Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,11 @@
+import { createClient } from 'redis';
 
+(async () => {
+    try {
+        const client = await createClient();
+        client.on('error', (err) => console.log('Redis client not connected to the server: ', err));
+        client.on('connect', () => console.log('Redis client connected to the server'));
+    } catch (error) {
+        console.error('Redis client not connected to the server: ', error);
+    }
+})();
