77f91ba92e903b64864789ffda05609bbf100c15
MahmoudEasa <mu01011422865@gmail.com>
2024-03-05 21:07:13 +0200

    Task 8

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,1 +1,14 @@
+import { createQueue } from 'kue';
 
+const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+}
+
+queue.process('push_notification_code', (job, done) => {
+    const { phoneNumber, message } = job.data;
+
+    sendNotification(phoneNumber, message);
+    done();
+});
--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -1,1 +1,73 @@
+import { createQueue } from 'kue';
 
+const queue = createQueue();
+
+const jobs = [
+    {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+    },
+    {
+        phoneNumber: '4153518781',
+        message: 'This is the code 4562 to verify your account'
+    },
+    {
+        phoneNumber: '4153518743',
+        message: 'This is the code 4321 to verify your account'
+    },
+    {
+        phoneNumber: '4153538781',
+        message: 'This is the code 4562 to verify your account'
+    },
+    {
+        phoneNumber: '4153118782',
+        message: 'This is the code 4321 to verify your account'
+    },
+    {
+        phoneNumber: '4153718781',
+        message: 'This is the code 4562 to verify your account'
+    },
+    {
+        phoneNumber: '4159518782',
+        message: 'This is the code 4321 to verify your account'
+    },
+    {
+        phoneNumber: '4158718781',
+        message: 'This is the code 4562 to verify your account'
+    },
+    {
+        phoneNumber: '4153818782',
+        message: 'This is the code 4321 to verify your account'
+    },
+    {
+        phoneNumber: '4154318781',
+        message: 'This is the code 4562 to verify your account'
+    },
+    {
+        phoneNumber: '4151218782',
+        message: 'This is the code 4321 to verify your account'
+    }
+];
+
+for (const el of jobs) {
+    const job = queue.create("push_notification_code_2", el);
+
+    job.on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+        job.remove();
+    });
+    
+    job.on('failed', (err) => {
+        console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (process) => console.log(`Notification job ${job.id} ${process}% complete`));
+    
+    job.save((err) => {
+        if (err) {
+            console.error('Error creating notification job:', err);
+        } else {
+            console.log(`Notification job created: ${job.id}`);
+        }
+    });
+}
